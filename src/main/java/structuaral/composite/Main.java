487db1ad81eac2afb5196121579f7c7a377927a9
Cao Ngoc Son <caongocson2122@gmail.com>
2021-12-28 22:54:04 +0700

    Add Composite Pattern (Copy)

---
+++ src/main/java/structuaral/composite/Main.java
@@ -0,0 +1,34 @@
+package structuaral.composite;
+
+import structuaral.composite.shapes.Circle;
+import structuaral.composite.shapes.CompoundShape;
+import structuaral.composite.shapes.Dot;
+import structuaral.composite.shapes.Rectangle;
+
+import java.awt.*;
+/*
+* Copy https://refactoring.guru/design-patterns/composite/java/example#example-0--Demo-java
+*
+* */
+public class Main {
+    public static void main(String[] args) {
+        ImageEditor editor = new ImageEditor();
+
+        editor.loadShapes(
+                new Circle(10, 10, 10, Color.BLUE),
+
+                new CompoundShape(
+                        new Circle(110, 110, 50, Color.RED),
+                        new Dot(160, 160, Color.RED)
+                ),
+
+                new CompoundShape(
+                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Dot(240, 240, Color.GREEN),
+                        new Dot(240, 360, Color.GREEN),
+                        new Dot(360, 360, Color.GREEN),
+                        new Dot(360, 240, Color.GREEN)
+                )
+        );
+    }
+}
